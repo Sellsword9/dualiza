d4ba619b1f58ae6c4b0195e0a5b527e733516fe9
Crixls <crixismundial@gmail.com>
2024-02-16 20:19:08 +0100

    Segundo commit cris

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,24 @@
+import { Link } from 'react-router-dom';
+
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+  return (
+    <header className="flex justify-between items-center p-4 bg-orange-custom text-white">
+      
+      <nav>
+          <ul style={{ listStyleType: 'none', padding: 0 }}>
+            <li style={{ display: 'inline', marginRight: '10px' }}><Link to="/">Proyecto</Link></li>
+            <li style={{ display: 'inline', marginRight: '10px' }}><Link to="/resultados">Resultados obtenidos</Link></li>
+            <li style={{ display: 'inline' }}><Link to="/mejora">Mejora tu centro</Link></li>
+            <li style={{ display: 'inline' }}><Link to="/noticias">Noticias</Link></li>
+            <li>oscurecer/aclarar</li> {/* Renderiza ThemeToggleButton como un componente */}
+          </ul>
+        </nav>
+    </header>
+  );
+};
+
+export default Header;
+
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {},
     colors: {
-      "orange-custom": "e75129"
+      "orange-custom": "#e75129",
+      
     }
   },
   plugins: [],
