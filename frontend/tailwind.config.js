68b262e30d9ae9fb7ff290cabdee440e9a651027
Crixls <crixismundial@gmail.com>
2024-02-16 20:25:46 +0100

    Commit colores

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,13 @@ export default {
   theme: {
     extend: {},
     colors: {
-      "orange-custom": "e75129"
+      "orange-custom": "#e75129",
+      "azul-semi-oscuro":"#314499",
+      "azul-oscuro":"#0f1875",
+      "azul-claro":"#0062d4",
+      "azul-negro":"#1d1b33",
+      "gris-oscuro":"#b3b3b3",
+      "gris-claro":"#ebebeb",
     }
   },
   plugins: [],
