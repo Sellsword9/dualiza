46958a8082670e352d4c616faa4bcd3eeefbd541
Yeray Romero <yerayromerodev@gmail.com>
2024-02-16 20:52:36 +0100

    Fixed colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,15 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      "orange-custom": "#e75129",
-      "azul-semi-oscuro":"#314499",
-      "azul-oscuro":"#0f1875",
-      "azul-claro":"#0062d4",
-      "azul-negro":"#1d1b33",
-      "gris-oscuro":"#b3b3b3",
-      "gris-claro":"#ebebeb",
+    extend: {
+      colors: {
+        "orange-custom": "#e75129",
+        "azul-semi-oscuro": "#314499",
+        "azul-oscuro": "#0f1875",
+        "azul-claro": "#0062d4",
+        "azul-negro": "#1d1b33",
+        "gris-oscuro": "#b3b3b3",
+        "gris-claro": "#ebebeb",
+      }
     }
   },
   plugins: [],
