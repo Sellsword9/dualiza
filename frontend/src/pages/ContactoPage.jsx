0f37c2d44ff8528ae9ac21eb774d2d3e1d60c342
Yeray Romero <yerayromerodev@gmail.com>
2024-02-17 16:57:33 +0100

    Testing email

--- frontend/src/pages/ContactoPage.jsx
+++ frontend/src/pages/ContactoPage.jsx
@@ -1,28 +1,37 @@
+import React, { useState } from 'react'
 import Logo from '../assets/LogoNegativoNoFondo.png'
+const email = import.meta.env.VITE_EMAIL;
+console.log(email);
 const ContactoPage = () => {
+  const [subject, setSubject] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.open(mailtoLink);
+    console.log(mailtoLink);
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen bg-gris-claro">
-      <div className="flex">
-        <div className="w-full max-w-md bg-gray-600 rounded-lg shadow-md p-8 border">
-          <h2 className="text-2xl font-bold mb-4">Contacto</h2>
-          <form>
-            <div className="mb-4">
-              <label htmlFor="name" className="block text-azul-oscuro font-bold mb-2">Nombre</label>
-              <input type="text" id="name" name="name" className="w-full px-3 py-2 border-8 rounded-md focus:outline-none focus:border-blue-500" />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="email" className="block text-azul-oscuro font-bold mb-2">Email</label>
-              <input type="email" id="email" name="email" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="message" className="block text-azul-oscuro font-bold mb-2">Mensaje</label>
-              <textarea id="message" name="message" rows="4" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"></textarea>
-            </div>
-            <button type="submit" className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600">Enviar</button>
-          </form>
-        </div>
-        <div className="ml-4 bg-gray-600">
-          <img src={Logo} alt="Logo" className="h-96" />
+    <div className='w-screen bg-gris-oscuro'>
+      <div className="w-2/5 bg-gray-600 rounded-lg p-8 flex-col m-auto">
+        <h2 className="text-4xl font-bold mb-4 text-white">Contacto</h2>
+        <form onSubmit={handleSubmit} >
+          <div className="mb-4">
+            <label htmlFor="asunto" className="block text-orange-custom font-bold mb-2 text-xl">Asunto</label>
+            <input type="text" id="asunto" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)}
+              className="w-full px-3 py-2 border-8 rounded-md focus:outline-none focus:border-blue-500" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block text-orange-custom font-bold mb-2 text-xl">Mensaje</label>
+            <textarea id="message" name="body" rows="4" value={body} onChange={(e) => setBody(e.target.value)}
+              className="w-full px-3 py-2 border-8 rounded-md focus:outline-none focus:border-blue-500"></textarea>
+          </div>
+          <button type="submit" className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600">Enviar</button>
+        </form>
+        <div className="mb-4 bg-gray-600 flex justify-center">
+          <img src={Logo} alt="Logo" className="h-full w-96" />
         </div>
       </div>
     </div>
