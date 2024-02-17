bed5c8f9724b0ed43c2670616a0b9a5e4512c71a
Crixls <crixismundial@gmail.com>
2024-02-17 15:23:31 +0100

    Commit proyectopage1

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,28 @@
 import { Link } from 'react-router-dom';
 import Logo from '../assets/LogoNegativoNoFondo.png'; // Importa la imagen del logo
 
+
 const Header = () => {
+  // Define el estado para controlar si el menú está abierto o cerrado
+
   return (
-    <header className="flex justify-between items-center p-4 bg-gradient-to-r from-azul-oscuro to-azul-negro text-white w-full h-40"> {/* Aumenta la altura a h-40 */}
+    <header className="flex justify-between items-center p-4 bg-gradient-to-r from-azul-oscuro to-azul-negro text-white w-full h-40">
       <div className='flex justify-between items-center w-full'>
-        <div>
-          <img src={Logo} className='h-40' alt="logo" /> {/* Aplica la clase h-40 directamente a la imagen */}
-        </div>
-        <div>
-          <nav>
-            <ul className="list-none p-6">
-              <li className="inline mr-6 bordeLinks bg-orange-custom py-3 text-white rounded-md lg:pl-16 pr-16"><Link to="/">Proyecto</Link></li>
-              <li className="inline mr-6 bordeLinks text-white bg-orange-custom py-3 rounded-md lg:pl-16 pr-16 "><Link to="/resultados">Resultados obtenidos</Link></li>
-              <li className="inline text-white mr-6 bordeLinks bg-orange-custom py-3 rounded-md lg:pl-16 pr-16"><Link to="/mejora">Mejora tu centro</Link></li>
-              <li className="inline text-white mr-6 bordeLinks bg-orange-custom py-3 rounded-md lg:pl-16 pr-16"><Link to="/noticias">Noticias</Link></li>
-              <li className='text-white inline '>oscurecer/aclarar</li> {/* Renderiza ThemeToggleButton como un componente */}
-            </ul>
-          </nav>
-        </div>
+        <img src={Logo} className='h-44' alt="logo" />
+
+        <nav>
+          <ul className="list-none p-6 flex">
+            <li className="inline mr-6 bordeLinks bg-orange-custom py-3 text-white rounded-md lg:pl-16 lg:pr-16 md:py-2 md:pl-4 md:pr-4 sm:py-2 sm:pl-2 sm:pr-2"><Link to="/">Proyecto</Link></li>
+            <li className="inline mr-6 bordeLinks text-white bg-orange-custom rounded-md lg:pl-16 lg:pr-16 md:py-2 md:pl-4 md:pr-4 sm:py-2 sm:pl-2 sm:pr-2"><Link to="/resultados">Resultados obtenidos</Link></li>
+            <li className="inline text-white mr-6 bordeLinks bg-orange-custom rounded-md lg:pl-16 lg:pr-16 md:py-2 md:pl-4 md:pr-4 sm:py-2 sm:pl-2 sm:pr-2"><Link to="/mejora">Mejora tu centro</Link></li>
+            <li className="inline text-white mr-6 bordeLinks bg-orange-custom rounded-md lg:pl-16 lg:pr-16 md:py-2 md:pl-4 md:pr-4 sm:py-2 sm:pl-2 sm:pr-2"><Link to="/noticias">Noticias</Link></li>
+            <li className='text-white inline'>oscurecer/aclarar</li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
 };
 
 export default Header;
+
--- frontend/src/pages/ProyectoPage.jsx
+++ frontend/src/pages/ProyectoPage.jsx
@@ -1,8 +1,69 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import albaytar from '../assets/proyectoImg/reunion albaytar.png';
+import gracia from '../assets/proyectoImg/reunion gracia.png';
+import hlanz from '../assets/proyectoImg/reunion hlanz.png';
 
 const ProyectoPage = () => {
-  return (
-    <div>ProyectoPage Cristina</div>
-  )
-}
+  // Configuración del slider
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true, // Habilita el autoplay
+    autoplaySpeed: 5000 // Establece la velocidad del autoplay en milisegundos (opcional)
+  };
 
-export default ProyectoPage
+  return (
+    <>
+      <div className="bg-gris-claro p-10 ">
+        <div className='m-10'>
+          <Slider {...settings}>
+            <div className='flex justify-center items-center'>
+              <img src={albaytar} alt="Slide 1" />
+            </div>
+            <div className='flex justify-center items-center'>
+              <img src={gracia} alt="Slide 2" />
+            </div>
+            <div className='flex justify-center items-center'>
+              <img src={hlanz} alt="Slide 3" />
+            </div>
+          </Slider>
+        </div>
+        <div className='flex m-32'>
+          <div className='p-7 mr-40 bg-white rounded-lg shadow-md shadow-azul-semi-oscuro'>
+            <h2 className='text-orange-custom text-2xl font-bold pb-6 text-center'>Descripción</h2>
+            <p className='text-justify '>
+                Los centros educativos deben liderar acciones para mejorar el uso de la energía,
+                en línea con los Objetivos de Desarrollo Sostenible de la Unión Europea. Su influencia
+                en el ahorro energético es crucial, ya que la formación de los alumnos promueve la conciencia 
+                ambiental. A través de la Convocatoria Ayudas Dualiza - Edición 2022, hemos obtenido recursos 
+                para realizar análisis energéticos en centros educativos. Compartiremos estos recursos contigo 
+                para identificar áreas de mejora y elaborar una guía de buenas prácticas.
+            </p>
+          </div>
+          <div className=' p-7 bg-white text-center rounded-lg shadow-md shadow-azul-semi-oscuro'> 
+            <h2 className='text-orange-custom text-2xl font-bold pb-4 text-center'>Objetivos</h2>
+            <p className='text-justify'>Al participar en este proyecto, alcanzarás objetivos clave: realizar análisis y auditorías energéticas 
+              en los centros educativos, involucrar al alumnado en mejorar la eficiencia energética y el mantenimiento
+               de las instalaciones, formar al profesorado y alumnado en temas de auditoría energética, explorar fuentes 
+               de energía renovable integradas en la arquitectura, establecer planes de acción ambiental con medidas concretas,
+                establecer redes colaborativas con empresas locales, y contribuir a una página web compartida para intercambiar
+                 buenas prácticas en ahorro energético y sostenibilidad.
+            </p>
+          </div>
+        </div>
+        <div className=' p-7 mr-32 ml-32 bg-white text-center rounded-lg shadow-md shadow-azul-semi-oscuro  text-white'>
+          <h2 className='text-white text-2xl font-bold pb-4'>Colaboradores</h2>
+
+        </div>
+      </div>
+    </>
+  );
+
+};
+
+export default ProyectoPage;
