e7d232b6a6c2d71f32af1d4adeff35b3459aabc6
CarlosAlvarez96 <calvarezmartin96@gmail.com>
2024-02-20 12:12:26 +0100

    noticias edit

--- frontend/src/components/NoticiasEdit.jsx
+++ frontend/src/components/NoticiasEdit.jsx
@@ -10,13 +10,13 @@ const NoticiasEdit = () => {
     <div>
       {esEditor && (
         <div className="bg-gris-claro flex flex-row justify-end p-4 ">
-          <button className="p-4   m-2 bg-orange-custom text-white hover:bg-orange-800 rounded-md border-solid ">
+          <button className="p-4  m-2 bg-orange-custom text-white hover:bg-orange-800 rounded-md border-solid ">
             Añadir noticias
           </button>
-          <button className="p-4   m-2 bg-blue-600 text-white hover:bg-blue-800 rounded-md border-solid ">
+          <button className="p-4  m-2 bg-blue-600 text-white hover:bg-blue-800 rounded-md border-solid ">
             Editar noticias
           </button>
-          <button className="p-4   m-2 bg-red-600 text-white hover:bg-red-800 rounded-md border-solid ">
+          <button className="p-4  m-2 bg-red-600 text-white hover:bg-red-800 rounded-md border-solid ">
             Borrar noticias
           </button>
         </div>
